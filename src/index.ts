fa4e0a9a518c066b6b797d9c057a2556cd184713
smhasanjamil <smhasanjamil23@gmail.com>
2025-05-13 00:19:32 +0600

    Solve problem for Array Filtering and Mapping, object manipulation, sorting, leap year checking, and finding maximum value

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ interface Person {
 }
 
 const people: Person[] = [
+  { name: "Reajul", age: 25, gender: "male" },
   { name: "Sumaiya", age: 25, gender: "female" },
   { name: "Hasan", age: 20, gender: "male" },
   { name: "Sajib", age: 22, gender: "male" },
@@ -17,11 +18,85 @@ const people: Person[] = [
   { name: "Jamil", age: 28, gender: "male" },
 ];
 
-function getMaleNames(persons: Person[]): string[] {
-  return persons
+const getMaleNames = (persons: Person[]): string[] =>
+  persons
     .filter((person) => person.gender === "male")
     .map((person) => person.name);
-}
 
 const maleNames = getMaleNames(people);
 console.log(maleNames);
+
+/**
+2.Task: Object Manipulation
+Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
+
+ */
+
+interface Book {
+  title: string;
+  author: string;
+  year: number;
+}
+
+const library: Book[] = [
+  { title: "Padma Nodir Majhi", author: "Manik Bandopadhyay", year: 1936 },
+  { title: "Lalsalu", author: "Syed Waliullah", year: 1948 },
+  { title: "Srikanta", author: "Sarat Chandra Chattopadhyay", year: 1917 },
+  { title: "Chokher Bali", author: "Rabindranath Tagore", year: 1903 },
+  { title: "Debdas", author: "Sarat Chandra Chattopadhyay", year: 1917 },
+];
+
+const extractTitles = (books: Book[]): string[] =>
+  books.map((book) => book.title);
+
+const bookTitles = extractTitles(library);
+console.log(bookTitles);
+
+/**
+4.Task: Sorting Objects
+Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array.
+ */
+
+interface Car {
+  make: string;
+  model: string;
+  year: number;
+}
+
+const carList: Car[] = [
+  { make: "Toyota", model: "Corolla", year: 2020 },
+  { make: "Honda", model: "Civic", year: 2018 },
+  { make: "Tesla", model: "Model 3", year: 2022 },
+  { make: "Ford", model: "F-150", year: 2015 },
+];
+
+const sortCarsByYear = (cars: Car[]): Car[] =>
+  [...cars].sort((a, b) => a.year - b.year);
+
+const sortedCars = sortCarsByYear(carList);
+console.log(sortedCars);
+
+/**
+6. Task: Leap Year Checker
+Write a function that determines whether a given year is a leap year.
+Example: Happy New Year
+ */
+
+const isLeapYear = (year: number): boolean =>
+  (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+const year = 2025;
+
+console.log(
+  isLeapYear(year) ? `${year} is a leap year!` : `${year} is not a leap year.`
+);
+
+/**
+08. Task: Find Maximum Value:
+Write a function that takes an array of numbers and returns the maximum value.
+ */
+
+const findMaxNumber = (numbers: number[]): number => Math.max(...numbers);
+
+const numberArray = [7, 5, 9, 11, 6];
+console.log(findMaxNumber(numberArray));
